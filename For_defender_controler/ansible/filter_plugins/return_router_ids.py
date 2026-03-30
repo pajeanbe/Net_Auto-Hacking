9de3c4b1b1199960133c5fdd77013c1b90711cd0
pajeanbetuto <pajeanbe@gmail.com>
2026-03-30 15:06:04 +0000

    init

--- For_defender_controler/ansible/filter_plugins/return_router_ids.py
+++ For_defender_controler/ansible/filter_plugins/return_router_ids.py
@@ -1,6 +1,6 @@
 def return_router_ids(output):
         # get all the router ID
-        return [line.split()[0] for line in output.split('\n')[2:-1]]
+        return list(set([line.split()[0] for line in output.split('\n')[2:-1]]))
 
 
 class FilterModule(object):
