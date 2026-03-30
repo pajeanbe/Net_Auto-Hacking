5a0ae44ea43cac41e890e4efd88ce2fed1ff111e
pajeanbetuto <pajeanbe@gmail.com>
2026-03-30 20:52:15 +0000

    init

--- For_defender_controler/ansible/filter_plugins/return_router_ids.py
+++ For_defender_controler/ansible/filter_plugins/return_router_ids.py
@@ -3,13 +3,9 @@ router_ids=["1.1.1.1","2.2.2.2","3.3.3.3","4.4.4.4"]
 def return_router_ids(output):
     output=[line.split()[0] for line in output.split('\n')[2:-1]]
     if not set(router_ids).issuperset(set(output)):
-        return ["OSPF list contain non valid router ID",output]
+        return ["OSPF list contain non valid router ID",list(set(output))]
     else:
         return list(set(output))
-
-        # get all the router ID
-        #return list(set([line.split()[0] for line in output.split('\n')[2:-1]]))
-
 
 class FilterModule(object):
     def filters(self):
