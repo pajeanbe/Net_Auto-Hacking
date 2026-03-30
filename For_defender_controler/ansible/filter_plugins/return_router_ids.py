9846b4309800eba7bc803e285b1a951573cb8823
pajeanbetuto <pajeanbe@gmail.com>
2026-03-30 19:29:05 +0000

    init

--- For_defender_controler/ansible/filter_plugins/return_router_ids.py
+++ For_defender_controler/ansible/filter_plugins/return_router_ids.py
@@ -1,6 +1,15 @@
+
+router_ids=["1.1.1.1","2.2.2.2","3.3.3.3","4.4.4.4"]
 def return_router_ids(output):
+    output=[line.split()[0] for line in output.split('\n')]
+    if not set(router_ids).issuperset(set(output)):
+        print("Router IDs not present in the output")
+        return "Error"
+    else:
+        return output
+
         # get all the router ID
-        return list(set([line.split()[0] for line in output.split('\n')[2:-1]]))
+        #return list(set([line.split()[0] for line in output.split('\n')[2:-1]]))
 
 
 class FilterModule(object):
