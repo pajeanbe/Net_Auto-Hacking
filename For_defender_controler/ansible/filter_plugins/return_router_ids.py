2525d8cce850af12306d5306e6fa9b9d80962c75
pajeanbetuto <pajeanbe@gmail.com>
2026-03-31 00:59:26 +0000

    init

--- For_defender_controler/ansible/filter_plugins/return_router_ids.py
+++ For_defender_controler/ansible/filter_plugins/return_router_ids.py
@@ -3,9 +3,10 @@ router_ids=["1.1.1.1","2.2.2.2","3.3.3.3","4.4.4.4"]
 def return_router_ids(output):
     output=[line.split()[0] for line in output.split('\n')[2:-1]]
     if not set(router_ids).issuperset(set(output)):
-        return True
+        return [True,list(set(output))] #contain a non-valid router_ID
     else:
-        return True
+        return [False,list(set(output))]
+
 
 class FilterModule(object):
     def filters(self):
